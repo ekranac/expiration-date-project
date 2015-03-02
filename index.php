25e1c10201beaf11a45302dde3076c27f1b15dfc
ekranac <besal.ziga@gmail.com>
2015-03-02 08:38:52 +0100

    Updated layout design

--- index.php
+++ index.php
@@ -39,16 +39,21 @@ if(isset($_POST['removeProduct']))
     <meta charset="UTF-8">
     <title>Document</title>
     
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
     <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href="css/style.css">
     
-    <script src="js/bootstrap.min.js" type="text/javascript"></script>
+    
     <script src="js/jquery-2.1.3.min.js" type="text/javascript"></script>
+    <script src="js/bootstrap.min.js" type="text/javascript"></script>
 </head>
 <body>
    <div class="container">
       <div class="row">
-           <form action="" method="post" id="add-form">
+          <div class="col-sm-4">
+             <h1 class="main-title">FOOD SAVER</h1>
+              <form action="" method="post" id="add-form">
                <fieldset>
                    <legend>Dodaj izdelek</legend>
                     <input type="text" name="title" id="title" placeholder="Naziv izdelka" class="form-control">
@@ -59,11 +64,37 @@ if(isset($_POST['removeProduct']))
                 </fieldset>
             </form>
             
-            <form action="" method="post">
-              <table style="width:70%;">
+            <br>
+            
+            <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+                 
+                <!-- Indicators -->
+                <ol class="carousel-indicators">
+                    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+                    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+                    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+                </ol>
+                <!-- Wrapper for slides -->
+                <div class="carousel-inner" role="listbox">
+                    <div class="item active">
+                        <img src="img/pic1.jpg">
+                    </div>
+                    <div class="item">
+                        <img src="img/pic2.jpg">
+                    </div>
+                    <div class="item">
+                        <img src="img/pic3.jpg">
+                    </div>
+                </div>
+            </div>
+
+          </div>
+          <div class="col-sm-8">
+              <form action="" method="post">
+              <table>
                   <tr>
                       <th>Naziv</th>
-                      <th>Datum poteka</th>
+                      <th colspan="2">Datum poteka</th>
                   </tr>
                   
                   <?php
@@ -83,6 +114,7 @@ if(isset($_POST['removeProduct']))
                   
               </table>
             </form>
+          </div>
         </div>
     </div>
 </body>
